83038ca689ec9ea53ef6eadfb10b09a0b5691150
Dola Barai <dola43dc@gmail.com>
2023-05-31 23:34:19 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 require('dotenv').config()
 app.use(cors());
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.send(401).send({error: true, message: 'unauthorized access'})
+  }
+  
+  const token = authorization.split(' ')[1];
 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+app.post('/jwt', (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+  res.send({token})
+})
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkm5by0.mongodb.net/?retryWrites=true&w=majority`;
@@ -41,22 +63,64 @@ async function run() {
         const result = await reviewsCollection.find().toArray();
         res.send(result)
     })
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result)
+  })
 
     //cart Collection
-    app.get('/carts', async(req, res) => {
+    app.get('/carts', verifyJWT, async(req, res) => {
       const email = req.query.email;
       if(!email){
         res.send([]);
       }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message: 'Forbidden access'})
+      }
+
       const query = {email: email}
       const result = await cartCollection.find(query).toArray()
       res.send(result)
-  })
+    })
 
     app.post ('/users', async(req, res) => {
       const user = req.body;
-      console.log(user);
+      
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+      
+      if(existingUser){
+        return res.send({message: 'user already exist'})
+      }
       const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+    // security layer: verifyJWT
+    // email same
+    app.get('/users/admin/:email', verifyJWT, async(req,res) => {
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin: false})
+      }
+      const query = {email: email}
+      const user = await userCollection.findOne(query)
+      const result = {admin: user?.role === 'admin'}
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      }
+      const result = await userCollection.updateOne(filter, updateDoc)
       res.send(result)
     })
 
